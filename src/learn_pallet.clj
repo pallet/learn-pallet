5cc884306edea6b6aed6aab930e562f427cd7aaf
Antoni Batchelli <tbatchelli@acm.org>
2014-02-05 15:50:20 -0800

    Enable the use of awaze for EC2 access.

--- src/learn_pallet.clj
+++ src/learn_pallet.clj
@@ -3,15 +3,17 @@
             [pallet.repl :refer :all]
             [pallet.compute :refer [instantiate-provider]]
             [pallet.crate.automated-admin-user :refer [with-automated-admin-user]]
-            [pallet.api :refer [compute-service]]))
+            [pallet.api :refer [compute-service node-spec]]))
 
 (def provider-deps
   {:vmfest '[[com.palletops/pallet-vmfest "0.4.0-SNAPSHOT"]]
-   :ec2 '[[org.cloudhoist/pallet-jclouds "1.5.2"]
+   :jcloud-ec2 '[[org.cloudhoist/pallet-jclouds "1.5.2"]
           [org.jclouds.provider/aws-ec2 "1.5.5"]
           [org.jclouds.provider/aws-s3 "1.5.5"]
           [org.jclouds.driver/jclouds-slf4j "1.5.5"]
-          [org.jclouds.driver/jclouds-sshj "1.5.5"]]})
+          [org.jclouds.driver/jclouds-sshj "1.5.5"]]
+   :pallet-ec2 '[[com.palletops/pallet-aws "0.1.1"]
+                 [org.slf4j/jcl-over-slf4j "1.7.5"]]})
 
 
 (defn distill-all
@@ -25,10 +27,13 @@
   *compute* nil)
 
 ;; ubuntu precise 64-bit EBS for us-east-1 region
-(def ec2-node-spec
-  {:image {:image-id "us-east-1/ami-e50e888c"}
-   :location { :location-id "us-east-1a"}})
+(def jclouds-ec2-node-spec
+  (node-spec :image {:image-id "us-east-1/ami-e50e888c"}
+             :location { :location-id "us-east-1a"}))
 
+(def pallet-ec2-node-spec
+  (node-spec :image {:image-id "ami-e50e888c"}
+             :location { :location-id "us-east-1a"}))
 
 (defn base-spec [node-spec]
   (pallet.api/group-spec "learn-pallet-base-spec"
@@ -55,8 +60,8 @@
       (alter-var-root #'*compute* (constantly compute))
       (apply bootstrap-vmfest *compute* flattened-opts))))
 
-(defn- load-ec2
-  "Sets EC2 as the compute provider"
+(defn- load-jclouds-ec2
+  "Sets jcloud's EC2 provider as the compute provider"
   [& {:keys [provider] :as opts}]
   (let [opts (dissoc opts :provider)
         flattened-opts (mapcat identity opts)
@@ -64,16 +69,30 @@
                   (apply compute-service provider opts)
                   (apply instantiate-provider :aws-ec2 flattened-opts))]
     (alter-var-root #'*compute* (constantly compute))
-    (alter-var-root #'*base-spec* (constantly (base-spec ec2-node-spec)))
+    (alter-var-root #'*base-spec* (constantly (base-spec jclouds-ec2-node-spec)))
     (println
-     "*** Congratulations! You're connected to Amazon Web Services. Enjoy!")))
+     "*** Congratulations! You're connected to Amazon Web Services via Apache jclouds. Enjoy!")))
+
+(defn- load-pallet-ec2
+  "Sets awaze's EC2 provider as the compute provider"
+  [& {:keys [provider] :as opts}]
+  (let [opts (dissoc opts :provider)
+        flattened-opts (mapcat identity opts)
+        compute (if provider
+                  (apply compute-service provider opts)
+                  (apply instantiate-provider :pallet-ec2 flattened-opts))]
+    (alter-var-root #'*compute* (constantly compute))
+    (alter-var-root #'*base-spec* (constantly (base-spec pallet-ec2-node-spec)))
+    (println
+     "*** Congratulations! You're connected to Amazon Web Services via Pallet Awaze. Enjoy!")))
 
 (defn bootstrap
   "Boostraps the project, based on the `provider`. Providers allowed are:
 
   - :vmfest -> for VirtualBox via XPCOM (native)
   - :vmfest-ws -> for VirtualBox via Web Services. Requires `vboxwebsrv` running.
-  - :ec2 -> for Amazon EC2. Requires :identity and :credential parameters."
+  - :jclouds-ec2 -> for Amazon EC2 via jclouds. Requires :identity and :credential parameters.
+  - :pallet-ec2 -> for Amazon EC2 via jclouds. Requires :identity and :credential parametes."
   [provider & opts]
   ;; pallet caches the list of available providers on the first run,
   ;; and doesn't update it afterwards. We load new providers at
@@ -85,8 +104,10 @@
                 (apply load-vmfest opts))
     :vmfest-ws (do (distill-all (:vmfest provider-deps))
                    (apply load-vmfest :vbox-comm :ws (rest opts)))
-    :ec2 (do (distill-all (:ec2 provider-deps))
-             (apply load-ec2 opts))))
+    :jclouds-ec2 (do (distill-all (:jclouds-ec2 provider-deps))
+                     (apply load-jclouds-ec2 opts))
+    :pallet-ec2 (do (distill-all (:pallet-ec2 provider-deps))
+                   (apply load-pallet-ec2 opts))))
 
 (defmacro bootstrap-ns
   "Bootstraps the namespace `ns` by downloading (if necessary) and
